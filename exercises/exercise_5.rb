b621a5c4e4451365477c98558a2fb7275a156336
Andrew <andrew.ku93@gmail.com>
2021-03-30 05:05:46 +0000

    exercise_5 calculating average sales per store and additional filtering

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+total_rev = Store.sum(:annual_revenue)
+puts "The company has a total revenue of #{total_rev}"
+
+total_stores = Store.count
+puts "Average revenue per store is #{total_rev/total_stores}"
+
+count_high_sales_stores = Store.where("annual_revenue > ?", 1000000).count
+puts "Number of stores that have greater than $1M of sales #{count_high_sales_stores}"
